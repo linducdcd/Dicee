81d0dd66a85d4703089a5006e2e3472b8b503bff
Mohammad Mehdi Azizi <linducdcd@gmail.com>
2019-05-02 14:47:25 -0700

    add Assets and completed first version of code

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomDiceR : Int = 0
+    var randomDiceL : Int = 0
+    
+    let diceArray = ["dice1" , "dice2" , "dice3" , "dice4" , "dice5" , "dice6" ]
+    
+    @IBOutlet weak var diceOutletR: UIImageView!
+    
+    @IBOutlet weak var diceOutletL: UIImageView!
+    
+    @IBAction func rollButton(_ sender: Any) {
+        updateDiceImage()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        updateDiceImage()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    func updateDiceImage() {
+        randomDiceR = Int(arc4random_uniform(6))
+        randomDiceL = Int(arc4random_uniform(6))
+        
+        
+        diceOutletL.image = UIImage(named: diceArray[randomDiceL])
+        diceOutletR.image = UIImage(named: diceArray[randomDiceR])
+        
+    }
 
 }
 
